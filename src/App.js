b2c40a48b006d74255662f919a159058262c8e49
tleul <leult2018@gmail.com>
2020-11-10 17:04:55 -0800

    filter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Route, Switch } from 'react-router-dom';
+import Catagory from './components/Catagory';
+import EventDetails from './components/EventDetails';
 import Events from './components/Events';
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 function App() {
 	return (
-		<div className='container mt-5'>
-			<Events />
-		</div>
+		<>
+			<Navbar />
+			<div className='container mt-5'>
+				<div class='d-flex flex-row  bd-highlight mb-3'>
+					<Switch>
+						<Route
+							path='/events/:id/:title'
+							component={EventDetails}
+						/>
+						<Route path='/' component={Home} />
+					</Switch>
+				</div>
+			</div>
+		</>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Router>
+			<App />
+		</Router>
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
